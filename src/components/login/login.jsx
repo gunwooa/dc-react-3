78a4b160f2f82f2a575799a8b2ce618e934f46dc
GunwooJang <gw.jang@cardoc.co.kr>
2020-12-26 23:36:35 +0900

    10.5 Route로 화면 전환

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import { useHistory } from "react-router-dom";
 import Header from "../header/header";
 import Footer from "../footer/footer";
 import styles from "./login.module.css";
+import { useEffect } from "react";
 
 const Login = ({ authService }) => {
+  const history = useHistory();
+  const goToMaker = (userId) => {
+    history.push({
+      pathname: `/maker`,
+      state: { id: userId },
+    });
+  };
+
   const onLogin = (event) => {
     authService //
       .login(event.currentTarget.textContent)
-      .then(console.log);
+      .then((data) => {
+        console.log(data);
+        goToMaker(data.user.uid);
+      });
   };
+
+  useEffect(() => {
+    authService //
+      .onAuthChange((user) => {
+        user && goToMaker(user.uid);
+      });
+  });
 
   return (
     <section className={styles.login}>
@@ -17,12 +36,20 @@ const Login = ({ authService }) => {
         <h1>Login</h1>
         <ul className={styles.list}>
           <li className={styles.item}>
-            <button className={styles.button} onClick={onLogin}>
+            <button
+              className={styles.button}
+              onClick={onLogin}
+              disabled={false}
+            >
               Google
             </button>
           </li>
           <li className={styles.item}>
-            <button className={styles.button} onClick={onLogin}>
+            <button
+              className={`${styles.button} ${styles.github}`}
+              onClick={onLogin}
+              disabled={true}
+            >
               Github
             </button>
           </li>
--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -3,8 +3,21 @@ import firebaseApp from "./firebase";
 
 class AuthService {
   login(providerName) {
+    console.log(`login: ${providerName}`);
     const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
     return firebaseApp.auth().signInWithPopup(authProvider);
+  }
+
+  onAuthChange(onUserChanged) {
+    console.log(`onAuthChange: ${onUserChanged}`);
+    firebase.auth().onAuthStateChanged((user) => {
+      onUserChanged(user);
+    });
+  }
+
+  logout() {
+    console.log(`logout`);
+    firebase.auth().signOut();
   }
 }
 
